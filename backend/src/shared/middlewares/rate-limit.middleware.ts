87a3fda718e140de5986f7643393871dbd47968a
Drummonds <drummond05@proton.md>
2026-02-12 18:48:55 -0300

    fix: rate limit interrupiting dev debugs

--- backend/src/shared/middlewares/rate-limit.middleware.ts
+++ backend/src/shared/middlewares/rate-limit.middleware.ts
@@ -1,22 +1,23 @@
 import rateLimit from "express-rate-limit";
+import { env } from "../../core/config/env";
 
 const ttl = 15 * 60 * 1000;
-const isTest = process.env.NODE_ENV === "test";
+const isProd = env.NODE_ENV === "production";
 
 export const authRateLimit = rateLimit({
   windowMs: ttl,
-  max: isTest ? 1000 : 5,
+  max: isProd ? 5 : 1000,
   message: "Muitas tentativas de login, tente novamente mais tarde",
 });
 
 export const registerRateLimit = rateLimit({
   windowMs: ttl,
-  max: isTest ? 1000 : 15,
+  max: isProd ? 10 : 1000,
   message: "Muitas tentativas de cadastro, tente novamente mais tarde",
 });
 
 export const globalRateLimit = rateLimit({
   windowMs: ttl,
-  max: isTest ? 1000 : 100,
+  max: isProd ? 100 : 1000,
   message: "Muitas requisições, tente novamente mais tarde",
 });
